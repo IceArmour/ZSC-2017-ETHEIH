60f10168f8504fa5fdd66b26449b42932e8bf1c7
ZHWKnight <china.wenhuang@gmail.com>
2018-04-25 20:47:58 +0800

    enter

---
+++ OpenCV3/CoordinateTransformation/CoordinateTransformation/main.cpp
@@ -0,0 +1,48 @@
+//--------------------------------------【程序说明】-------------------------------------------
+//		程序说明：第四次汇报 CoordinateTransformation
+//		程序描述：坐标变换
+//		开发测试所用操作系统：Windows 10 64bit
+//		开发测试所用IDE版本：Visual Studio 2017
+//		开发测试所用OpenCV版本：3.4.0
+//		2018年3月14日 Created by @ZHWKnight
+//		2018年4月14日 Revised by @ZHWKnight
+//---------------------------------------------------------------------------------------------
+
+#include <opencv.hpp>
+#include <highgui.hpp>
+#include <imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+
+	Mat srcImage = imread("1.jpg");
+	Mat grayImage, gaussImage, gAhresImage;
+	cvtColor(srcImage, grayImage, COLOR_RGB2GRAY);
+
+	GaussianBlur(grayImage, gaussImage, Size(5, 5), 0, 0);
+	threshold(gaussImage, gAhresImage, 60, 255, 0);
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(gAhresImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	drawContours(gAhresImage, contours, -1, Scalar(128), 2);
+
+	vector<Moments> mu(contours.size());
+	for (int i = 0; i < contours.size(); i++)
+		mu[i] = moments(contours[i], true);
+	vector<Point2d> mc(contours.size());
+	for (int i = 0; i < contours.size(); i++)
+		mc[i] = Point2f(static_cast<float>(mu[i].m10 / mu[i].m00), static_cast<float>(mu[i].m01 / mu[i].m00));
+	for (int i = 0; i < contours.size(); i++)
+		circle(gAhresImage, mc[i], 3, 128, -1);
+
+	imshow("原始图像", srcImage);
+	imshow("效果图", gAhresImage);
+
+	waitKey(0);
+	return(0);
+}
+
---
+++ README.md
@@ -0,0 +1,45 @@
+# ZSC-2017-ETHEIH-with-OpenCV3 <br /> 基于机器视觉的工业机器人位置伺服控制研究
+
+## 总览
+
+本项目由学院老师牵头，参与人员是本科学生。采用了 OpenCV 视觉库进行开发，主要使用语言是 C++ 。
+
+使用工业相机配合现有的六轴机械手进行研究，涉及的相关技术有：
+- C++ 程序语言设计
+- MATLAB 自然科学语言
+- 数据结构
+- 计算机图像处理
+- 数字、模拟电路设计
+- 可编程逻辑门电路设计
+- 运动控制理论
+
+## 贡献
+
+由于项目组成员经验与实力的原因，问题颇多，还望各位多多指教。
+如果您愿意提出宝贵修改，敬请留意以下几点：
+- OpenCV 版本目前为 3.4.1 
+- 缩进格式采用 Tab 制表符
+
+## 教程
+面向项目组的教程：
+1. 注册 "Github" 账号，Fork or Clone 项目。
+2. 下载 "Git" 客户端和 "Visual Studio Code" 客户端
+3. 安装并运行 "Git Bash"
+4. 执行命令 `git config --global user.name "XXX"` 修改用户名
+5. 执行命令 `git config --global user.email "XXX@XXX"` 修改用户邮箱
+6. 执行命令 `cd x:/xxx/` 切换到工作目录
+7. 执行命令 `git init` 在当前目录新建仓库
+8. 执行命令 `git config --global credential.helper store` 开启存储密码功能
+9. 执行命令 `git remote add etheih_github XXX` 添加远程仓库
+10. 执行命令 `git pull etheih_github master` 拉取仓库到本地
+11. 执行命令 `git add OpenCV3` 添加项目文件夹到监视表 
+12. 运行 "Visual Studio Code" 检查源代码管理标签
+
+## 反馈
+
+ZSCEyeinHand@foxmail.com
+
+## 更新列表
+v1.0 2018/04/16 Hello World!  
+v1.1 2018/04/17 新增指导教程
+v1.2 2018/04/25 修改错别字，重新建库
